b9f9d90c6b638e80b0cec08943046c65c201604f
Jan Bernitt <jan.bernitt@gmx.de>
2013-02-19 20:39:52 +0100

    more about production rules

--- src/se/jbee/build/Producer.java
+++ src/se/jbee/build/Producer.java
@@ -2,7 +2,7 @@ package se.jbee.build;
 
 public interface Producer {
 	// fluent interface example
-	// produce(class).from(java); 
-	// produce(jar).from(class);
-	// produce(javadoc).from(java);
+	// produce(class).from(java).with(javac); // class and java are artifacts (that can be filtered as well) 
+	// produce(jar).from(class).with(jar);
+	// produce(javadoc).from(java).with(javadoc);
 }
